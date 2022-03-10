e32cff5dfdbf03da71dfe969a46ba4a2d08525f7
Zaytov3n <s133514301@gmail.com>
2022-03-10 06:29:03 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO,"and that piece of art is useful"
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char ch[]= "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
 	return (1);
 }
